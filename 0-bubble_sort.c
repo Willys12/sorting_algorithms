dd52be83a6dafded68c4ebe9eaf03eb0d6a92d8e
Willys12 <ben.willys92@gmail.com>
2024-02-18 13:34:13 -0800

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,53 +1,5 @@
 #include "sort.h"
-
-/*Helper function*/
-void print_arr(int *array, size_t size);
-void swap(int *a, int *b);
-/**
-* print_arr - Print an array on integers.
-* @array: Pointer to array of integers.
-* @size: Number of elements in array.
-* Return: Nothing.
-*/
-void print_arr(int *array, size_t size)
-{
-size_t length, i = 0;
-char buffer[12];
-char *ptr;
-
-for (; i < size; i++)
-{
-if (i > 0)
-write(1, ", ", 2);
-
-ptr = buffer + sizeof(buffer) - 1;
-*ptr = '\0';
-
-do 
-{
-ptr--;
-*ptr = '0' + (array[i] % 10);
-array[i] /= 10;
-}while (array[i] != 0);
-length = buffer + sizeof(buffer) - ptr - 1;
-write(1, ptr, length);
-}
-write(1, "\n", 1);
-}
-
-/**
-* swap - Swaps integers.
-* @a: Points to first integer.
-* @b: Points to the second integer.
-* Return: Nothing
-*/
-void swap(int *a, int *b)
-{
-int temp = *a;
-*a = *b;
-*b = temp;
-}
-
+ 
 /**
 * bubble_sort - sorts an array of integers in ascending order.
 * @array: Points to the array to be sorted.
@@ -56,18 +8,20 @@ int temp = *a;
 */
 void bubble_sort(int *array, size_t size)
 {
-size_t m = 0;
 size_t n = 0;
+size_t m = 0;
+int temp;
 
-for (; m < size - 1; m++)
+for (; n < size - 1; n++)
 {
-for (; n < size - m - 1; n++)
+for (; m < size - n - 1; m++)
 {
-if (array[n] > array[n + 1])
+if (array[m] > array[m + 1])
 {
-swap(&array[n], &array[n + 1]);
+temp = array[m];
+array[m] = array[m + 1];
+array[m + 1] = temp;
 }
 }
-print_arr(array, size);
 }
 }
