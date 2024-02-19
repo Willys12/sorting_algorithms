05dd6dae2e6e65feaba76ef2afb096be92135281
Willys12 <ben.willys92@gmail.com>
2024-02-19 01:10:16 -0800

    Bubble sort: Print array updated

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,4 @@
-#include "sort.h"
-
-/*Helper function*/
-void print_arr(int n);
-
-/**
-* print_arr - Prints array of integers to standard output.
-* @n:Integer to print
-* Return: Nothing
-*/
-void print_arr(int n)
-{
-char fgr;
-
-if (n < 0)
-{
-write(1, "-", 1);
-n = -n;
-}
-if (n / 10)
-{
-print_arr(n / 10);
-}
-fgr = (n % 10) + '0';
-write(1, &fgr, 1);
-}
- 
+#include "sort.h" 
 /**
 * bubble_sort - sorts an array of integers in ascending order.
 * @array: Points to the array to be sorted.
@@ -33,31 +7,33 @@ write(1, &fgr, 1);
 */
 void bubble_sort(int *array, size_t size)
 {
-size_t j = 0;
-size_t n = 0;
 size_t m = 0;
+size_t n = 0;
+bool swapped;
 int temp;
+
+if (array == NULL || size < 2)
+{
+return;
+}
 
 for (; n < size - 1; n++)
 {
+swapped = false;
 for (; m < size - n - 1; m++)
 {
 if (array[m] > array[m + 1])
-{
-temp = array[m];
-array[m] = array[m + 1];
-array[m + 1] = temp;
-
-for (; j < size; j++)
-{
-print_arr(array[j]);
-if (j != size - 1)
-{
-write(1, " ", 1);
+	{
+	temp = array[m];
+	array[m] = array[m + 1];
+	array[m + 1] = temp;
+	swapped = true;
+	print_array(array, size);
+	}
 }
-}
-write(1, "\n", 1);
-}
+if (!swapped)
+{
+break;
 }
 }
 }
